be7141d09ef10d766613391a6f229c2f2bb09123
torusn <nicholastorus@gmail.com>
2024-03-21 18:35:03 -0400

    Enable TS ensure installed

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  -- ensure_installed = { "c", "lua", "rust", "go", "lua" },
+  ensure_installed = { "c", "lua", "rust", "go", "lua", "vimdoc", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   -- sync_install = false,
