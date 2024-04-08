25b0eedd27ceeb90a365abcc6a8800f067eb0f88
torusn <nicholastorus@gmail.com>
2024-04-07 21:43:44 -0400

    Find hidden files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,16 @@
+local telescope = require 'telescope'
 local builtin = require('telescope.builtin')
+
+telescope.setup {
+    pickers = {
+        find_files = {
+            find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
+        },
+    },
+}
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")})
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
