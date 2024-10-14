28d78063e5e30acdd490345b4447783c66c80db7
torusn <nicholastorus@gmail.com>
2024-10-13 21:13:11 -0400

    telescope: Add current hovered word search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -14,3 +14,17 @@ vim.keymap.set('n', '<C-f>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fs', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+
+-- find all of words under cursor
+vim.keymap.set('n', '<leader>fw', function()
+    local word = vim.fn.expand("<cword>")
+
+    builtin.grep_string({ search = word })
+end)
+
+-- find all of words under cursor delimited by whitespace
+vim.keymap.set('n', '<leader>fW', function()
+    local word = vim.fn.expand("<cWORD>")
+
+    builtin.grep_string({ search = word })
+end)
